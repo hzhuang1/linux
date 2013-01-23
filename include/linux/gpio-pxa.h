9afb48cdc4de9d943db1abaee72c6c0a8b9eea8c
Haojian Zhuang <haojian.zhuang@linaro.org>
2013-01-23 10:56:00 +0800

    gpio: pxa: avoid to use global irq base
    Avoid to use global irq_base in gpio-pxa driver. Define irq_base in each
    pxa_gpio_chip instead. Then we can avoid to use macro PXA_GPIO_TO_IRQ() &
    MMP_GPIO_TO_IRQ().
    
    Signed-off-by: Haojian Zhuang <haojian.zhuang@linaro.org>

--- include/linux/gpio-pxa.h
+++ include/linux/gpio-pxa.h
@@ -1,6 +1,8 @@
 #ifndef __GPIO_PXA_H
 #define __GPIO_PXA_H
 
+#include <linux/irq.h>
+
 #define GPIO_bit(x)	(1 << ((x) & 0x1f))
 
 #define gpio_to_bank(gpio)	((gpio) >> 5)
@@ -11,7 +13,7 @@
  */
 extern int pxa_last_gpio;
 
-extern int pxa_irq_to_gpio(int irq);
+extern int pxa_irq_to_gpio(struct irq_data *d);
 
 struct pxa_gpio_platform_data {
 	bool ed_mask;	/* true means that ed_mask reg is available */
